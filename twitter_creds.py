d0b3486016c0a101a61ff44edb9150228c0de42e
bakabrooks <brooks.alexr@gmail.com>
2020-03-05 09:36:42 -0800

    Initial commit

---
+++ twitter_creds.py
@@ -0,0 +1,6 @@
+# Query Twitter API for each tweet in the Twitter archive and save JSON in a text file
+# These are hidden to comply with Twitter's API terms and conditions
+consumer_key = '6WMZnnCRZEtfclwkGCwhNjnMF'
+consumer_secret = 'PBKad5bTPEcHH6Ulnv8LVExBZH8qCoci9XALxKsWmdlwc2hgXr'
+access_token = '2649540547-7HFAxVHr2Mh65ihLbfTHgExKB1R2LQQUyjzjof8'
+access_secret = '1vJaHrGVT5ZLrqGh83lWZVci1czYCe8YdRCvFYScWPtNg'
